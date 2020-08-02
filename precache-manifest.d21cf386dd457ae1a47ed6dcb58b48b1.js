704477fd047bb436493c7fb1681aa468c25f0976
mixxwaa <cookies765@gmail.com>
2020-08-02 12:17:20 +0200

    Updates

---
+++ precache-manifest.d21cf386dd457ae1a47ed6dcb58b48b1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e4ff49b3eb92b82d2bde1039c25389",
+    "url": "/madbarz-task/index.html"
+  },
+  {
+    "revision": "4d2f3f2985c78b561d78",
+    "url": "/madbarz-task/static/js/2.79ab58a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/madbarz-task/static/js/2.79ab58a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1773d6cea945f0e804",
+    "url": "/madbarz-task/static/js/main.297d4007.chunk.js"
+  },
+  {
+    "revision": "cb7da81754cdaec94bec",
+    "url": "/madbarz-task/static/js/runtime-main.d4c97ab1.js"
+  },
+  {
+    "revision": "8e76e78bd351e1f2269c0c58e2baaf88",
+    "url": "/madbarz-task/static/media/Ellipse.8e76e78b.svg"
+  },
+  {
+    "revision": "aa81e8dac757b6d5a6b3d026b3f1c044",
+    "url": "/madbarz-task/static/media/clock-feed.aa81e8da.svg"
+  },
+  {
+    "revision": "21b68c55ffd3d7d870b099fa1999407f",
+    "url": "/madbarz-task/static/media/comment-feed.21b68c55.svg"
+  },
+  {
+    "revision": "7b67aedc3fca16c5e0e5aaada48c3544",
+    "url": "/madbarz-task/static/media/fire-trending.7b67aedc.svg"
+  },
+  {
+    "revision": "da310472c2eac0816bc2f4f6d1e4a5f2",
+    "url": "/madbarz-task/static/media/like active.da310472.svg"
+  },
+  {
+    "revision": "9d721effb9057576443c3ab14906d35d",
+    "url": "/madbarz-task/static/media/like-feed.9d721eff.svg"
+  },
+  {
+    "revision": "ca86a661b41787a66868123fa4918c79",
+    "url": "/madbarz-task/static/media/loader.ca86a661.svg"
+  },
+  {
+    "revision": "6414d04950929a666b0299340dca7d38",
+    "url": "/madbarz-task/static/media/logo.6414d049.svg"
+  },
+  {
+    "revision": "04f1a5aac89405ea25bad1bff24429e5",
+    "url": "/madbarz-task/static/media/open-menu.04f1a5aa.svg"
+  },
+  {
+    "revision": "945eaed41748569d4b7d33eeea3cd78b",
+    "url": "/madbarz-task/static/media/share-feed.945eaed4.svg"
+  }
+]);
