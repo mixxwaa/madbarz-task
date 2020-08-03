6801b4862d9c05bf65429aa9b693a2988901fa31
mixxwaa <cookies765@gmail.com>
2020-08-03 15:25:39 +0200

    added reducer update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
-import * as TYPE from '../actions/types';
-
 const respectReducer = (state, action) => {
   switch (action.type) {
-    case TYPE.RESPECT_WORKOUT:
+    case 'RESPECT_WORKOUT':
       return {
         ...state,
         [action.id]: {
@@ -14,7 +12,7 @@ const respectReducer = (state, action) => {
           },
         },
       };
-    case TYPE.UNRESPECT_WORKOUT:
+    case 'UNRESPECT_WORKOUT':
       return {
         ...state,
         [action.id]: {
@@ -33,24 +31,13 @@ const respectReducer = (state, action) => {
 
 const feedReducer = (state = { feed: {}, loading: true }, action) => {
   switch (action.type) {
-    case TYPE.FETCH_FEED_DATA:
+    case 'SET_USERS':
       return { ...state, feed: action.payload };
-    case TYPE.UPDATE_FEED_DATA:
-      return {
-        ...state,
-        feed: {
-          ...state.feed,
-          ids: [...state.feed.ids, ...action.payload.ids],
-          featuredCount:
-            state.feed.featuredCount + action.payload.featuredCount,
-          byId: { ...state.feed.byId, ...action.payload.byId },
-        },
-      };
-    case TYPE.LOADING_START:
+    case 'LOAD_USERS':
       return { ...state, loading: true };
-    case TYPE.LOADING_FINISHED:
+    case 'FINISHED_LOADING':
       return { ...state, loading: false };
-    case TYPE.RESPECT_WORKOUT:
+    case 'RESPECT_WORKOUT':
       return {
         ...state,
         feed: {
@@ -58,7 +45,7 @@ const feedReducer = (state = { feed: {}, loading: true }, action) => {
           byId: respectReducer(state.feed.byId, action),
         },
       };
-    case TYPE.UNRESPECT_WORKOUT:
+    case 'UNRESPECT_WORKOUT':
       return {
         ...state,
         feed: {
